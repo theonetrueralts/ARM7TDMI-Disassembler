7eee3b75de572980677df52382085d627e0e3ea0
theonetrueralts <theonetrueralts@outlook.com>
2025-06-29 04:30:53 -0500

    Modified main.cpp into a small command line interface.

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,159 @@
 ﻿#include <iostream>
-#include <cstdint>
+#include <format>
+#include <string>
+#include <filesystem>
 #include <vector>
 #include <span>
-#include <format>
+#include <unordered_map>
+#include <optional>
+#include <fstream>
 
+#include <totr/disassembler/Common.hpp>
 #include <totr/disassembler/ArmDisasm.hpp>
 #include <totr/disassembler/ThumbDisasm.hpp>
-#include <totr/disassembler/Common.hpp>
-#include <unordered_map>
+#include <totr/disassembler/FileUtil.hpp>
+#include <totr/disassembler/InstructionProbe.hpp>
 
 namespace td = totr::Disassembler;
 
-enum class Mode { ARM, THUMB };
+void print_instruction(std::ostream& out, td::InstructionData data, td::ArmMode mode, bool ambiguous, bool mode_switched, bool did_mode_override) {
+    out << std::format("#0x{:08X}", data.pc) << " : ";
 
-void print_instruction(td::InstructionData data, Mode mode, bool did_mode_override, bool ambiguous) {
-    std::cout << std::format("#0x{:04X}", data.pc) << " : ";
-
-    std::cout << std::format("#0x{:08X}", data.instruction);
+    out << std::format("#0x{:08X}", data.instruction);
     
-    std::cout << " : " << data.mnemonic;
+    out << " : " << data.mnemonic;
     if (ambiguous) {
-        if (mode == Mode::ARM) std::cout << " ; May switch to THUMB.";
-        else std::cout << " ; May switch to ARM.";
+        if (mode == td::ArmMode::ARM) out << " ; May switch to THUMB.";
+        else out << " ; May switch to ARM.";
     }
     else if (did_mode_override) {
-        if (mode == Mode::ARM) std::cout << " ; Mode force switched to ARM.";
-        else std::cout << " ; Mode force switched to THUMB.";
+        if (mode == td::ArmMode::ARM) out << " ; Mode force switched to ARM.";
+        else out << " ; Mode force switched to THUMB.";
+    }
+    else if (mode_switched) {
+        if (mode == td::ArmMode::ARM) out << " ; Mode switch to ARM detected.";
+        else out << "; Mode switch to THUMB detected.";
     }
 
-    std::cout << std::endl;
+    out << "\n";
 }
 
-std::uint32_t read_word32_at(std::span<const std::uint8_t> rom, std::uint32_t pc) {
-    if (pc + 3 >= rom.size()) {
-        return std::uint32_t(rom[pc]) | (std::uint32_t(rom[pc + 1]) << 8);
-    }
-    return std::uint32_t(rom[pc])          |
-        (std::uint32_t(rom[pc + 1]) << 8)  |
-        (std::uint32_t(rom[pc + 2]) << 16) |
-        (std::uint32_t(rom[pc + 3]) << 24);
+void usage(const char* exe) {
+    std::cout
+        << "Usage: " << exe << " <rom file> [options]\n\n"
+        << "Options:\n"
+        << "  -r, --override <file>  Path to mode-override table\n"
+        << "  -o, --out <file>       Write disassembly to <file> instead of stdout\n"
+        << "  -d, --dec              Print immediates in decimal (default: hex)\n"
+        << "\nExamples:\n"
+        << "  " << exe << " example.rom\n"
+        << "  " << exe << " example.rom --dec\n"
+        << "  " << exe << " example.rom -r overrides.txt -o dump.txt\n";
 }
 
-int main() {
-    std::vector <std::uint8_t> opcodes = {
-        0x00, 0x00, 0xA0, 0xE3, // 0x00 : MOV R0, #0
-        0x00, 0x10, 0xA0, 0xE3, // 0x04 : MOV R1, #0
-        0x01, 0x00, 0x80, 0xE2, // 0x08 : ADD R0, R0, #1
-        0x02, 0x10, 0x81, 0xE2, // 0x0C : ADD R1, R1, #2
-        0x01, 0x30, 0x8F, 0xE2, // 0x10 : ADR R3, #25
-        0x13, 0xFF, 0x2F, 0xE1, // 0x14 : BX R3
-        0x03, 0x30,             // 0x18 : ADD R0, #3
-        0x04, 0x31,             // 0x1A : ADD R1, #4
-        0x02, 0xA3,             // 0x1C : ADR R3, #8
-        0x18, 0x47,             // 0x1E : BX R3
-        0x05, 0x00, 0x80, 0xE2, // 0x20 : ADD R0, R0, #5
-        0x06, 0x10, 0x81, 0xE2, // 0x24 : ADD R1, R1, #6
-        0xFE, 0xFF, 0xFF, 0xEA  // 0x28 : B #40
-    };
-
-    std::unordered_map<std::uint32_t, Mode> mode_override_table = {
-        { 0x18, Mode::THUMB },
-        { 0x20, Mode::ARM }
-    };
-
-    std::span<const uint8_t> rom{ opcodes };
+int main(int argc, char* argv[]) {
+    // CLI Init
+    if (argc < 2) { usage(argv[0]); return 1; }
 
     bool print_literals_hex = true;
+    std::filesystem::path rom_path = argv[1];
+    std::optional<std::filesystem::path> out_path;
+    std::optional<std::filesystem::path> override_path;
+
+    for (int i = 2; i < argc; ++i) {
+        std::string_view arg = argv[i];
+
+        if (arg == "-d" || arg == "--dec") {
+            print_literals_hex = false;
+        }
+        else if (arg == "-o" || arg == "--out") {
+            if (i + 1 == argc) { usage(argv[0]); return 1; }
+            out_path = argv[++i];
+        }
+        else if (arg == "-r" || arg == "--override") {
+            if (++i == argc) { usage(argv[0]); return 1; }
+            override_path = argv[i];
+        }
+        else {
+            std::cerr << "Unknown option: " << arg << '\n';
+            usage(argv[0]); return 1;
+        }
+    }
+
+    std::ofstream outfile;
+    std::ostream* out = &std::cout;
+    if (out_path) {
+        outfile.open(*out_path, std::ios::out | std::ios::trunc);
+        if (!outfile) {
+            std::cerr << "Cannot write to " << *out_path << "\n";
+            return 2;
+        }
+        out = &outfile;
+    }
+
+    auto opcodes = td::load_rom(rom_path.string());
+    std::unordered_map<std::uint32_t, td::ArmMode> mode_override_table;
+    if (override_path) mode_override_table = td::load_overrides(override_path->string());
+
+    // Main loop
+    std::span<const uint8_t> rom{ opcodes };
     td::ArmDisasm d_arm{ print_literals_hex };
     td::ThumbDisasm d_thumb{ print_literals_hex };
 
     int pc = 0;
-
-    Mode mode = Mode::ARM;
+    td::ArmMode mode = td::ArmMode::ARM;
     bool did_mode_override = false;
+    bool mode_switched = false;mode_switched = false;
     bool ambiguous = false;
 
     std::uint32_t instr = 0;
     td::InstructionData data;
 
-    std::cout << "  Addr   : Instr   : Mnemonic" << std::endl;
-    std::cout << "--------------------------------" << std::endl;
+    *out << "    Addr    :    Instr    : Mnemonic\n";
+    *out << "--------------------------------------\n";
     
     while (pc < rom.size()) {
         ambiguous = false;
+        mode_switched = false;
         did_mode_override = false;
 
-        instr = read_word32_at(rom, pc);
+        instr = td::read_word32_at(rom, pc);
 
-        if (mode == Mode::ARM) data = d_arm.decode(pc, instr);
+        if (mode == td::ArmMode::ARM) data = d_arm.decode(pc, instr);
         else data = d_thumb.decode(pc, instr);
+
+        pc += data.size;
 
         if (data.mode_event == td::ModeEvent::BX) {
             ambiguous = true;
+
+            td::ModeGuess guess = probe_mode(rom, pc, d_arm, d_thumb);
+
+            switch (guess) {
+                case td::ModeGuess::ARM:
+                    mode = td::ArmMode::ARM;
+                    mode_switched = true;
+                    ambiguous = false;
+                    break;
+                case td::ModeGuess::THUMB:
+                    mode = td::ArmMode::THUMB;
+                    mode_switched = true;
+                    ambiguous = false;
+                    break;
+                default: break;
+            }
         }
         else if (data.mode_event == td::ModeEvent::ExceptionReturn) {
-            mode = Mode::ARM;
+            mode = td::ArmMode::ARM;
         }
         
-        pc += data.size;
         if (auto it = mode_override_table.find(pc); it != mode_override_table.end()) {
             mode = it->second;
             did_mode_override = true;
             ambiguous = false;
         }
 
-        print_instruction(data, mode, did_mode_override, ambiguous);
+        print_instruction(*out, data, mode, ambiguous, mode_switched, did_mode_override);
     }
 
     return 0;
